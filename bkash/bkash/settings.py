d95b35fcdb72d990110d37d45daac7df9ed20924
Abdullah Al Noman <1705088@ugrad.cse.buet.ac.bd>
2020-10-07 12:10:37 +0600

    django

--- bkash/bkash/settings.py
+++ bkash/bkash/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = '=@@79#o5l_-t)3y12b0jnmkc*v(9rea5g*4p7^z+$zbn6=-9p&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'dashboard.apps.DashboardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
